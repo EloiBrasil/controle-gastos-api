df8d034a0aeed6f2e120e12dec23fff32a6ef508
EloiBrasil <eloigabriel0404@gmail.com>
2026-01-29 19:42:45 -0300

    Real commit

---
+++ controle-de-Gastos-Pessoais/src/test/java/com/example/controle_de_Gastos_Pessoais/ControleDeGastosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controle_de_Gastos_Pessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeGastosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
